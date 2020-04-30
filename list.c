45a17655f38b197ab483df111f2ad64a14b8b252
sukhiboi <40167376+sukhiboi@users.noreply.github.com>
2020-04-30 19:43:03 +0530

    Created add_to_end function

--- list.c
+++ list.c
@@ -18,3 +18,19 @@ List_ptr create_list(void)
     list->count = 0;
     return list;
 }
+
+Status add_to_end(List_ptr list, int value)
+{
+    Node_ptr new_node = create_node(value);
+    if (list->head == NULL)
+    {
+        list->head = new_node;
+    }
+    else
+    {
+        list->last->next = new_node;
+    }
+    list->last = new_node;
+    list->count++;
+    return Success;
+}
